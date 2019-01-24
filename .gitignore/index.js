9c855eb4aabd65d3d74ccddfd6b9b8b6fd7e81cb
KannaSlash <46974153+KannaSlash@users.noreply.github.com>
2019-01-24 16:51:27 +0100

    Create index.js

---
+++ .gitignore/index.js
@@ -0,0 +1,10 @@
+const Discord = require('discord.js');
+const fs = require('fs')
+const bot = new Discord.Client();
+var prefix = '!'
+bot.on('ready', function(){
+    bot.user.setGame("Command: :help");
+    console.log("Connected")
+})
+
+client.login('NTM3NjU3OTI3MzE4NjM0NDk4.DyofPA.S7qRiNOmNtKS1ZrB1JXCTFWBQeM');
